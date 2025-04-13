f1985a60ad837cd494837fdaf26e82bf4c73770b
VoltriiiiiiixZ <neogateau@gmail.com>
2025-04-13 13:05:57 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # autorise les requêtes cross-origin
+
+# Utilisateurs fictifs (pour test)
+users = {
+    "entrepriseX": "voici"
+}
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if users.get(username) == password:
+        return jsonify(success=True, message="Connexion réussie !")
+    else:
+        return jsonify(success=False, message="Identifiants invalides"), 401
